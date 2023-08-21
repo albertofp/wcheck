01efe1ac74c2f7d0641641002b4a98f8cf1235bb
Alberto Pluecker <albertopluecker@gmail.com>
2023-08-21 21:32:47 +0200

    API response struct, basic printing

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -18,9 +19,7 @@ func main() {
 	city := "Berlin"
 	API_KEY := os.Getenv("WEATHERAPI_KEY")
 
-	fmt.Printf("API KEY: %s\n", API_KEY)
-
-	query := fmt.Sprintf("http://api.weatherapi.com/v1/current.json?key=%s&q=%s&aqi=no", API_KEY, city)
+	query := fmt.Sprintf("http://api.weatherapi.com/v1/forecast.json?key=%s&q=%s&days=1&aqi=no&alerts=no", API_KEY, city)
 	res, err := http.Get(query)
 	if err != nil {
 		panic(err)
@@ -35,5 +34,38 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(string(body))
+	var weather Weather
+	err = json.Unmarshal(body, &weather)
+	if err != nil {
+		panic(err)
+	}
+
+	location, current, _ := weather.Location, weather.Current, weather.Forecast.Forecastday[0]
+
+	fmt.Printf("%s, %s: %.0fC, %s\n", location.Name, location.Country, current.TempC, current.Condition.Text)
+}
+
+type Weather struct {
+	Location struct {
+		Name    string `json:"name"`
+		Country string `json:"country"`
+	} `json:"location"`
+	Current struct {
+		TempC     float64 `json:"temp_c"`
+		Condition struct {
+			Text string `json:"text"`
+		} `json:"condition"`
+	} `json:"current"`
+	Forecast struct {
+		Forecastday []struct {
+			Hour []struct {
+				TimeEpoch int64   `json:"time_epoch"`
+				TempC     float64 `json:"temp_c"`
+				Condition struct {
+					Text string `json:"text"`
+				} `json:"condition"`
+				ChanceOfRain float64 `json:"chance_of_rain"`
+			} `json:"hour"`
+		} `json:"forecastday"`
+	} `json:"forecast"`
 }
